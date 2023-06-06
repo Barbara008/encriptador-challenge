a477b2fe0b831399d00b3889a9f0a772ef388dac
Barbara <5mendozacacheuta@gmail.com>
2023-06-06 16:56:26 -0300

    creacion de la llamada al evento en el btn 'Encriptar' y llamado a la funcion 'encriptador'

--- js/script.js
+++ js/script.js
@@ -1,24 +1,56 @@
-const encriptar = document.querySelector('.encriptar');
-const desencriptar = document.querySelector('.desencriptar');
-let texto = document.querySelector('.text');
-let textoAEncriptar = texto.value;
+let btnEncriptar = document.querySelector('.encriptar');
+let txtEncriptar;
+let noText = document.querySelector('.no-text');
+let yesText = document.querySelector('.yes-text');
+let respuesta = document.querySelector('.respuesta');
 
-let fraseCambiada = '';
-function encriptador() {
+// ENCRIPTADOR
 
-    fraseCambiada = textoAEncriptar.replaceAll('a','ai').replaceAll('e','enter').replaceAll('i','imes').replaceAll('o','ober').replaceAll('u','ufat');
+/**
+ * Retorna encriptado el mensaje enviado por el usuario
+ */
 
-    return fraseCambiada;
+function encriptador(texto) {
 
+    return texto.replaceAll(/a/gi,'ai').replaceAll(/e/gi,'enter').replaceAll(/i/gi,'imes').replaceAll(/o/gi,'ober').replaceAll(/u/gi,'ufat');
+
+}
+
+
+// cambian el estado de la caja de respuesta
+function campoVacio() {
+    noText.classList.remove('of')
+    yesText.classList.add('of')
+}
+
+function campoLleno() {
+    noText.classList.add('of');
+    yesText.classList.remove('of')
+}
+
+btnEncriptar.addEventListener('click',(e)=>{
+    e.preventDefault();
+    txtEncriptar = document.querySelector('.text').value;
     
-}
+    if (txtEncriptar == "") {
+        campoVacio()
+    }else{
+        campoLleno()
+        respuesta.innerHTML = encriptador(txtEncriptar);
+    }
+    
+})
 
-function desencriptador() {
-    fraseDesencriptada = fraseCambiada.replaceAll("ufat","u").replaceAll('ober','o').replaceAll('imes','i').replaceAll('enter','e').replaceAll('ai','a')
 
-    return fraseDesencriptada;
+// DESENCRIPTADOR
 
-}
+/**
+ * Retorna desencriptado el mensaje que fue previamente encriptado
+ */
 
+// function desencriptador() {
+//     let fraseDesencriptada = fraseCambiada.replaceAll(/ufat/gi,"u").replaceAll(/ober/gi,'o').replaceAll(/imes/gi,'i').replaceAll(/enter/gi,'e').replaceAll(/ai/gi,'a')
 
+//     return fraseDesencriptada;
 
+// }
